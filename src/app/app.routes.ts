a188372cab38d341b09a79d22db488f5b7f5a19f
Njyhier <njerinjihia43@gmail.com>
2025-11-10 12:24:44 +0300

    Configure the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RepoDetails } from './repo-details/repo-details';
+import { Landing } from './landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: Landing},
+  {path:'repo-details', component: RepoDetails}
+];
