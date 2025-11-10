f9862b92b9be5879506dfb23c586713f5e8a5d05
Njyhier <njerinjihia43@gmail.com>
2025-11-10 12:30:11 +0300

    Update the imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
-import { Landing } from './landing/landing';
+import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Landing, FormsModule, RouterLinkWithHref],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
