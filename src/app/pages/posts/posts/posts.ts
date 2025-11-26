1bb8af9ce3dd64a4cf98997d778eed8d6f3758d8
crispus-nj <njengacris250@gmail.com>
2025-11-26 16:27:56 +0300

    updated posts

--- src/app/pages/posts/posts/posts.ts
+++ src/app/pages/posts/posts/posts.ts
@@ -1,16 +1,112 @@
 import { Component, inject } from '@angular/core';
 import { AppService } from '../../../services/app/app-service';
+import { JsonPipe } from '@angular/common';
+import { IPost } from '../../../models/post.interface';
+import { concatMap, map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './posts.html',
   styleUrl: './posts.css',
 })
 export class Posts {
   appService: AppService = inject(AppService);
 
+  posts: IPost[] = [];
+
   constructor() {
-    console.log(this.appService.testCrispus({ name: 'Crispus', age: 30 }));
+    // this.listPosts();
+    // this.fetchDataUsingConcatMap();
+    this.fetchDataUsingSwithcMap();
   }
+
+  fetchDataUsingConcatMap(): void {
+    this.appService
+      .listPosts()
+      ?.pipe(
+        map((posts) => {
+          console.log(posts, 'Fetched Posts before mapping IDs');
+          this.posts = posts;
+          return posts.map((post) => post.id);
+        })
+      )
+      ?.pipe(
+        concatMap((posts) => {
+          console.log(posts, 'Mapped Post IDs');
+          return this.listUsers();
+        })
+      )
+      ?.pipe(concatMap((users) => this.listComments()))
+      // ?.pipe(map((comments) => console.log(comments)))
+      ?.subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => console.log(err),
+      });
+  }
+  fetchDataUsingSwithcMap(): void {
+    this.appService
+      .listPosts()
+      // ?.pipe(
+      //   map((posts) => {
+      //     console.log(posts, 'Fetched Posts before mapping IDs');
+      //     this.posts = posts;
+      //     return posts.map((post) => post.id);
+      //   })
+      // )
+      ?.pipe(
+        switchMap((posts) => {
+          console.log(posts, 'Mapped Post IDs');
+          return this.listUsers();
+        })
+      )
+      ?.pipe(switchMap((users) => this.listComments()))
+      // ?.pipe(map((comments) => console.log(comments)))
+      ?.subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => console.log(err),
+      });
+  }
+
+  listUsers(): Observable<any> {
+    return this.appService.listUsers();
+  }
+
+  listComments(): Observable<any> {
+    return this.appService.listComments();
+  }
+  listPosts(): Observable<IPost[]> {
+    return this.appService.listPosts();
+  }
+  // listPosts(): void {
+  //   this.appService.listPosts().subscribe({
+  //     next: (res) => {
+  //       this.posts = res;
+  //       console.log(this.posts);
+  //     },
+  //     error: (err) => console.log(err),
+  //   });
+  // }
+
+  // listUsers(): void {
+  //   this.appService.listUsers().subscribe({
+  //     next: (res) => {
+  //       console.log(res);
+  //     },
+  //     error: (err) => console.log(err),
+  //   });
+  // }
+
+  // listComments(): void {
+  //   this.appService.listComments().subscribe({
+  //     next: (res) => {
+  //       console.log(res);
+  //     },
+  //     error: (err) => console.log(err),
+  //   });
+  // }
 }
--- src/app/services/app/app-service.ts
+++ src/app/services/app/app-service.ts
@@ -19,9 +19,36 @@ export class AppService {
    * List user posts. 
    * @author Crispus Njenga
    * @returns Observable<IPost[]>
+   * @alias listPosts
+   * @memberof AppService
+   * @example
+   * ```typescript
+   * this.appService.listPosts().subscribe({
+   *  next: (res) => {
+   *    console.log(res);
+   *  },
+   *  error: (err) => console.log(err),
+   * });
+   * ```  
    */
   listPosts(): Observable<IPost[]> {
     return this.httpClient.get<IPost[]>("https://jsonplaceholder.typicode.com/posts")
+  }
+
+  /**
+   * List users
+   * @returns Observable<any>
+   */
+  listUsers(): Observable<any> {
+    return this.httpClient.get<any>("https://jsonplaceholder.typicode.com/use")
+  }
+
+  /**
+   * List comments
+   * @returns Observable<any>
+   */
+  listComments(): Observable<any> {
+    return this.httpClient.get<any>("https://jsonplaceholder.typicode.com/comments")
   }
 
   /**
